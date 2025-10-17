f566be1bd1bed9d581597b44f116dfb416642c2d
_FN10_ <mathdudex09@gmail.com>
2025-10-16 21:19:37 -0300

    change to hcaptcha

--- src/main/java/fn10/server/servlet/ApiServlet.java
+++ src/main/java/fn10/server/servlet/ApiServlet.java
@@ -1,19 +1,24 @@
 package fn10.server.servlet;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.entity.EntityBuilder;
+import org.apache.hc.client5.http.entity.UrlEncodedFormEntity;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.ClassicHttpResponse;
 import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.Header;
 import org.apache.hc.core5.http.HttpEntity;
 import org.apache.hc.core5.http.HttpException;
+import org.apache.hc.core5.http.NameValuePair;
 import org.apache.hc.core5.http.io.HttpClientResponseHandler;
+import org.apache.hc.core5.http.message.BasicNameValuePair;
 import org.eclipse.jetty.http.HttpStatus;
 
 import com.google.gson.Gson;
@@ -76,9 +81,11 @@ public class ApiServlet extends HttpServlet {
         public CapchaVerifyRequest(String s, String re) {
             this.secret = s;
             this.response = re;
-            /*if (mote != null)
-                if (mote.isBlank())
-                    this.remoteip = mote;*/ //lets not send the ip of everyone to google :)
+            /*
+             * if (mote != null)
+             * if (mote.isBlank())
+             * this.remoteip = mote;
+             */ // lets not send the ip of everyone to google :)
         }
     }
 
@@ -100,26 +107,42 @@ public class ApiServlet extends HttpServlet {
 
             case "capchaValid":
                 try {
-                    String clientKey = new String(request.getInputStream().readAllBytes());
+                    String clientKey = new String(request.getInputStream().readAllBytes()).trim();
                     if (clientKey.isBlank()) {
                         response.setStatus(HttpStatus.BAD_REQUEST_400);
                         break;
                     }
 
                     CloseableHttpClient client = HttpClients.createDefault();
-                    HttpPost post = new HttpPost("https://www.google.com/recaptcha/api/siteverify");
+                    HttpPost post = new HttpPost("https://hcaptcha.com/siteverify");
 
-                    
+                    /*
+                     * post.setEntity(EntityBuilder.create().setContentType(ContentType.
+                     * APPLICATION_JSON)
+                     * .setText(gson.toJson(
+                     * new CapchaVerifyRequest(Files.readString(Path.of(App.path,
+                     * "secretCapchaKey.txt")),
+                     * clientKey)))
+                     * .build());
+                     */
 
-                    post.setEntity(EntityBuilder.create().setContentType(ContentType.APPLICATION_JSON)
-                            .setText(gson.toJson(
-                                    new CapchaVerifyRequest(Files.readString(Path.of(App.path, "secretCapchaKey.txt")),
-                                            clientKey)))
-                            .build());
+                    List<NameValuePair> params = new ArrayList<NameValuePair>();
+                    params.add(new BasicNameValuePair("secret",
+                            Files.readString(Path.of(App.path, "secretCapchaKey.txt")).trim()));
+                    params.add(new BasicNameValuePair("response",
+                            clientKey));
+
+                    post.setHeader("Content-Type", "application/x-www-form-urlencoded");
+                    post.setHeader("Accept", "application/json");
+
+                    post.setEntity(new UrlEncodedFormEntity(params));
 
                     System.out.println("(" + getClass().getSimpleName() + ") "
-                            + "Sending POST to https://www.google.com/recaptcha/api/siteverify, with params: "
-                            + new String(post.getEntity().getContent().readAllBytes()));
+                            + "Sending POST to https://hcaptcha.com/siteverify. Headers: ");
+                    for (Header header : post.getHeaders()) {
+                        System.out.println(header.getName() + ": " + header.getValue());
+                    }
+                    // + new String(post.getEntity().getContent().readAllBytes()));
 
                     response.setContentType("text/plain");
                     response.getWriter().println(client.execute(post, new HttpClientResponseHandler<String>() {
